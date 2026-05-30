6436f094e819086133791b1511bd6a588f345514
LuisLeon1705 <leonpizzano@gmail.com>
2026-05-30 12:09:50 -0400

    Arreglo de direccion

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
+const isVercel = process.env.VERCEL === '1';
 const repo = 'LuisLeon1705';
 
 const nextConfig: NextConfig = {
@@ -8,8 +9,8 @@ const nextConfig: NextConfig = {
   output: "export",
 
   // 2. Set the Base Path for GitHub Pages
-  // In production (GitHub), we need the repo name. In development, we use the root.
-  basePath: isProd ? `/${repo}` : '',
+  // In production (GitHub), we need the repo name. In development or Vercel, we use the root.
+  basePath: (isProd && !isVercel) ? `/${repo}` : '',
 
   // 3. Disable Image Optimization (GitHub Pages doesn't support the image optimization server)
   images: {
